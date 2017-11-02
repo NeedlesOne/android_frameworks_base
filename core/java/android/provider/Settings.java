0af9c9caf644dbd226bf7a35fa9950dda609fb8e
Raj Yengisetty <rajesh@cyngn.com>
2017-11-02 18:23:21 -0400

    base: Fix - place Clear Recents button above the nav bar
    base: Fix up 'clear recents' UX
    
    Use the 'dismiss all' button drawable and place the button in the upper
    right corner. Doing so fixes the broken animation when going into
    recents
    
    base: Recents panel clear all button tweaks [1/2]
    
    - Show or hide recents button
    - Place the button in top right or left
    
    Change-Id: I9a96011a5136ecc41af5772e8e37fd59af9e423f
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4667,6 +4667,18 @@ public final class Settings {
         public static final String QS_QUICKBAR_COLUMNS = "qs_quickbar_columns";
 
         /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
          * Three Finger Gesture from Oppo
          * @hide
          */
