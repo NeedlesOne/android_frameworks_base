3b32cf972da31da433efe7aa2dc7a9fd7e401fa5
Miguel Martinez <slverwolf@gmail.com>
2018-03-24 15:41:29 -0500

    Incall vibration options [1/3]
    Change-Id: I4c0ee012eabb2bc7cc42334921155cc854af9f96

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3999,6 +3999,24 @@ public final class Settings {
         public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+        /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+        /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * @hide
          */
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
