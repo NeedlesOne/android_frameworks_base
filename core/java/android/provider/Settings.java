eda625ae292ffe98ccdda24846a325204ea224dd
Miguel Martinez <slverwolf@gmail.com>
2018-05-13 15:53:22 -0500

    Revert "Import mtp_dirty_hack [1/2]"
    This reverts commit 9a39e0097a9ef560dae63c8849350d49abec0f95.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3952,13 +3952,6 @@ public final class Settings {
          */
 
         // Omni additions start system
-		
-		/**
-         * Whether to use the MTP by default after connecting to PC
-         * @hide
-         */
-        public static final String MTP_DIRTY_HACK = "mtp_dirty_hack";
-		
         /**
          * @hide
          */
--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -39,7 +39,6 @@ import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothProfile.ServiceListener;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -71,7 +70,6 @@ import android.os.Message;
 import android.os.Parcel;
 import android.os.RemoteException;
 import android.os.ResultReceiver;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.telephony.CarrierConfigManager;
@@ -1037,7 +1035,6 @@ public class Tethering extends BaseNetworkObserver {
 
         synchronized (mPublicSync) {
             if (enable) {
-                SystemProperties.set("mtp_hack", "true");
                 if (mRndisEnabled) {
                     final long ident = Binder.clearCallingIdentity();
                     try {
@@ -1051,7 +1048,6 @@ public class Tethering extends BaseNetworkObserver {
                     usbManager.setCurrentFunction(UsbManager.USB_FUNCTION_RNDIS, false);
                 }
             } else {
-                SystemProperties.set("mtp_hack", "");
                 final long ident = Binder.clearCallingIdentity();
                 try {
                     tetherMatchingInterfaces(IControlsTethering.STATE_AVAILABLE,
